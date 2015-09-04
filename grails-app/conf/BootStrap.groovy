02efda7fe4be10f265414a92829d7205fb46a457
taborde <taborde@NCI-S076378-L.nci.nih.gov>
2015-09-04 15:11:08 -0400

    removing biospecimenType view(s)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,57 +5,53 @@ import nci.bbrb.cdr.util.AppSetting
 class BootStrap {
 
     def init = { servletContext ->
-    def adminRole = new nci.bbrb.cdr.authservice.CdrRole(authority: 'ROLE_ADMIN').save(failOnError: false, flush: true)
-   // def userRole  = new nci.bbrb.cdr.authservice.CdrRole(authority: 'ROLE_USERS').save(failOnError: false, flush: true)
-    def prcRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_PRC').save(failOnError: false, flush: true)
-     def uuuRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_BSS_UUU').save(failOnError: false, flush: true)
-     def cccRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_BSS_CCC').save(failOnError: false, flush: true)
+        def adminRole = new nci.bbrb.cdr.authservice.CdrRole(authority: 'ROLE_ADMIN').save(failOnError: false, flush: true)
+       // def userRole  = new nci.bbrb.cdr.authservice.CdrRole(authority: 'ROLE_USERS').save(failOnError: false, flush: true)
+        def prcRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_PRC').save(failOnError: false, flush: true)
+        def uuuRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_BSS_UUU').save(failOnError: false, flush: true)
+        def cccRole   = new nci.bbrb.cdr.authservice.CdrRole(authority:   'ROLE_BSS_CCC').save(failOnError: false, flush: true)
 
-    def adminUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'admin', enabled: true, password: 'admin')
-    adminUser.save(failOnError: false, flush: true)
-    
-     CdrUserRole.create(adminUser, adminRole, true)
-     
-    def uuuUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testeruuu', enabled: true, password: 'Welcome01')
-    uuuUser.save(failOnError: false, flush: true)
-    
-    CdrUserRole.create(uuuUser, uuuRole, true)
-    
-    def cccUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testerccc', enabled: true, password: 'Welcome01')
-    cccUser.save(failOnError: false, flush: true)
-    
-    CdrUserRole.create(cccUser, cccRole, true)
-        
-   // def testUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testUser', enabled: true, password: 'pmhtest')
-    //testUser.save(failOnError: false, flush: true)
+        def adminUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'admin', enabled: true, password: 'admin')
+        adminUser.save(failOnError: false, flush: true)
 
-    //CdrUserRole.create(testUser, userRole, true)
+        CdrUserRole.create(adminUser, adminRole, true)
+
+        def uuuUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testeruuu', enabled: true, password: 'Welcome01')
+        uuuUser.save(failOnError: false, flush: true)
+
+        CdrUserRole.create(uuuUser, uuuRole, true)
     
-    //assert CdrUser.count() == 2
-    //assert CdrRole.count() == 2
-    //assert CdrUserRole.count() == 2
+        def cccUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testerccc', enabled: true, password: 'Welcome01')
+        cccUser.save(failOnError: false, flush: true)
+
+        CdrUserRole.create(cccUser, cccRole, true)
+
+        // def testUser = new nci.bbrb.cdr.authservice.CdrUser(username: 'testUser', enabled: true, password: 'pmhtest')
+        //testUser.save(failOnError: false, flush: true)
+
+        //CdrUserRole.create(testUser, userRole, true)
+
+        //assert CdrUser.count() == 2
+        //assert CdrRole.count() == 2
+        //assert CdrUserRole.count() == 2
     
-    println "cdrUser count ="+CdrUser.count()
-    println "cdrRole count ="+CdrRole.count()
-    println "cdrUserRole count ="+CdrUserRole.count()
-    
-        
-        
-        
-          
+        println "cdrUser count ="+CdrUser.count()
+        println "cdrRole count ="+CdrRole.count()
+        println "cdrUserRole count ="+CdrUserRole.count()
+
         new AppSetting(code: "HELP_EMAIL", name: "HELP_EMAIL", value: "pushpa.hariharan@nih.gov", bigValue: "").save(failOnError: false, flush: true)
         new AppSetting(code: "loginbulletin", name: "loginbulletin", value: "see big value", bigValue: "this is the login bulletin message").save(failOnError: false, flush: true)
         //pmh 05/04/15 
         new AppSetting(code: "CDRLITE_USER_DENY_DISEASE", name: "Deny user access for Disease study. ", value: "see big value", bigValue: "testDUser").save(failOnError: false, flush: true)
         new AppSetting(code: "CDRLITE_ADMIN_DISTRO", name: "CDRLITE_ADMIN_DISTRO", value: "see big value", bigValue: "pushpa.hariharan@nih.gov").save(failOnError: false, flush: true)
         new AppSetting(code: "QUERY_RESPONSE_DCC_DM", name: "QUERY_RESPONSE_DCC_DM", value: "see big value", description: "Exclude responses from local DCC DM users in this list from being counted in the AR query tracker report", bigValue: "admin").save(failOnError: false, flush: true)
-         
+
         new Study(code: 'BPS', name:'BPS').save(failOnError: false, flush: true)
-        
+
         new BSS(code:'UUU', name:'UUU').save(failOnError: false, flush: true)
         new BSS(code:'CCC', name:'CCC').save(failOnError: false, flush: true)
-       
-        
+
+
         new CaseCollectionType(code: 'POSTM', name:'Postmortem').save(failOnError: false, flush: true)
         new CaseCollectionType(code: 'OPO', name:'Organ Donor (OPO)').save(failOnError: false, flush: true)
         new CaseCollectionType(code: 'SURGI', name:'Surgical').save(failOnError: false, flush: true)
@@ -72,12 +68,12 @@ class BootStrap {
         new CaseStatus(code:'BSSQACOMP', name:'BSS QA Review Complete', description: 'QA review at BSS is complete. Case is locked from further data entry.').save(failOnError: false, flush: true)
         new CaseStatus(code:'INIT', name:'Initialized', description: '').save(failOnError: false, flush: true)
         new CaseStatus(code:'WDCPROG', name:'Case Recall in Progress', description: 'Case Recall in Progress').save(failOnError: false, flush: true)
-        
+
         new TissueType(code:'KIDNEY', name:'Kidney' ).save(failOnError: false, flush: true)
         new TissueType(code:'COLON', name:'Colon' ).save(failOnError: false, flush: true)
         new TissueType(code:'LUNG', name:'Lung' ).save(failOnError: false, flush: true)
         new TissueType(code:'OVARY', name:'Ovary' ).save(failOnError: false, flush: true)
-        
+
         new ContainerType(code:'SST',       name:'SST Vacutainer' ).save(failOnError: false, flush: true)
         new ContainerType(code:'LAVEDTA',   name:'Lavender EDTA' ).save(failOnError: false, flush: true)
         new ContainerType(code:'CRYOV',     name:'Cryovial' ).save(failOnError: false, flush: true)
@@ -92,9 +88,9 @@ class BootStrap {
         new ContainerType(code:'ACD',       name:'ACD Vacutainer' ).save(failOnError: false, flush: true)
         new ContainerType(code:'BUCKET',    name:'Bucket' ).save(failOnError: false, flush: true)
         new ContainerType(code:'ENVELOPE',  name:'Envelope' ).save(failOnError: false, flush: true)
-        
+
         new Fixative(code:"OCT", name:"OCT Embedded").save(failOnError: false, flush: true)
-        
+
         /* Tabor: Add generic Data Coordinating Center and University */
         new Organization(code:'DCC', name:'Data Coordinating Center').save(failOnError: false, flush: true)
         new Organization(code:'UUU', name:'Some University').save(failOnError: false, flush: true)
@@ -102,7 +98,7 @@ class BootStrap {
         /*
         def  orgUUU = new CdrRole(authority: 'ROLE_ORG_UUU').save(failOnError: false, flush: true)
         def  orgDCC = new CdrRole(authority: 'ROLE_ORG_DCC').save(failOnError: false, flush: true)
-        
+
         CdrUserRole.create(testUser, orgUUU, true)            
         CdrUserRole.create(adminUser, orgDCC, true)   
         */
@@ -112,9 +108,13 @@ class BootStrap {
         new ActivityType(code:'CASEUPDATE', name:'CASE UPDATED' ).save(failOnError: false, flush: true)
         new ActivityType(code:'STATUSCHG', name:'CASE STATUS CHANGE' ).save(failOnError: false, flush: true)
         
-        
+        new QueryType(code:'MISS',    name:'Missing Data' ).save(failOnError: false, flush: true)
+        new QueryType(code:'DISCREP', name:'Discrepant Data' ).save(failOnError: false, flush: true)
+        new QueryType(code:'VERIFY',  name:'Verify Data' ).save(failOnError: false, flush: true)
+        new QueryType(code:'ACTION',  name:'Action' ).save(failOnError: false, flush: true)
         
     }
+    
     def destroy = {
     }
 }
